a657f5e3ffde1a43397db7ae0d1a8fe0f08178d2
Rob Godwin <thegodwinator@gmail.com>
2019-10-09 09:09:48 -0400

    added main method comment

--- src/com/robgodwin/Life.java
+++ src/com/robgodwin/Life.java
@@ -1,6 +1,13 @@
 package com.robgodwin;
 
 public class Life {
+	
+	/**
+	 * A version of Conway's Game of Life created in Java using Swing
+	 * @Author Rob Godwin
+	 * @Version 1.0
+	 * @Date October 9, 2019
+	 */
 
 	public static void main(String[] args) {
 		new Frame();
